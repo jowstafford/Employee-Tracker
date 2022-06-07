ec79486bcd0c9667907be7e5a76fa79fb3d624ac
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-07 18:56:08 -0500

    added the inputResult function in index.js

--- index.js
+++ index.js
@@ -11,3 +11,49 @@ var connection = mysql.createConnection({
 connection.connect(function () {
   inputResult();
 });
+
+function inputResult() {
+  inquirer
+
+    .prompt({
+      name: "task",
+      type: "list",
+      message: "Choose an option:",
+      choices: [
+        "view employees",
+        "add employee",
+        "remove employee",
+        "add role",
+        "update employees role",
+        "exit",
+      ],
+    })
+
+    .then(function ({ task }) {
+      switch (task) {
+        case "view employees":
+          viewCommand();
+          break;
+
+        case "add employee":
+          addCommand();
+          break;
+
+        case "remove employee":
+          removeCommand();
+          break;
+
+        case "add role":
+          addCommand();
+          break;
+
+        case "update employees role":
+          updateCommand();
+          break;
+
+        case "exit":
+          connection.end();
+          break;
+      }
+    });
+}
