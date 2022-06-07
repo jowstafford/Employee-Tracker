ccfd45ba87fac7c10b4b22274470f46a93a00185
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-07 18:30:16 -0500

    added values into the role table

--- sql/seed.sql
+++ sql/seed.sql
@@ -8,3 +8,14 @@ INSERT INTO department (name)
 VALUES ("Engineering");
 INSERT INTO department (name)
 VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 60000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 100000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 120000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 80000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 160000, 4);
