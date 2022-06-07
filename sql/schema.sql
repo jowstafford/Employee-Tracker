042c2df4dbb5d550d30026511a4ae8222f77c8dc
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-07 15:08:54 -0500

    created the employee table

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS employeesdb;
 CREATE DATABASE employeesdb;
 
 USE employeesdb;
+
+CREATE TABLE employee (
+  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+);
